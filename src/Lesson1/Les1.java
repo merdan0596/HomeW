1b75467e4a939680f3faa5c10a33893b743bec8e
merdan0596 <merdan.bashimov96@mail.ru>
2018-01-07 06:27:47 +0300

    Lesson1

---
+++ src/Lesson1/Les1.java
@@ -0,0 +1,8 @@
+package Lesson1;
+
+public class Les1 {
+    public static void main(String[] args) {
+        System.out.println("Как я провел лето 2017");
+    }
+}
+
---
+++ src/Lesson2/Les2.java
@@ -0,0 +1,7 @@
+package Lesson2;
+
+public class Les2 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
